4ca827fbfc08a10ba28e0f87829d187c1885a722
Özlem Yıldız <yldz.ozlm@gmail.com>
2018-08-12 00:14:22 +0300

    Changes all around.

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,8 @@
 // dev.js - production keys here!!
 module.exports = {
-  googleClientID : process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
+  googleClientID:
+    '415314963859-nk0kvqj72121e93hmo4ekksr8d5apl3g.apps.googleusercontent.com',
+  googleClientSecret: '6iMplZmUkoCOdGusTCum67Z4',
+  mongoURI: 'mongodb://oezlem:password1@ds247191.mlab.com:47191/oy_emaily_dev',
+  cookieKey: ''
 };
